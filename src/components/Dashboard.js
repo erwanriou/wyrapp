568013c11001b5d2a267c1e467ae93b10757fbf7
Erwan Riou <rr.1.riou@gmail.com>
2018-06-08 18:35:03 +0200

    implementing toggle function in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,22 @@ import { connect } from 'react-redux'
 import Question from './Question.js'
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isToggle: false }
+    this.handleToggleQuestions = this.handleToggleQuestions.bind(this);
+    this.handleToggleAnswers = this.handleToggleAnswers.bind(this);
+  }
+
+  handleToggleQuestions = (e) => {
+    e.preventDefault()
+    this.setState({isToggle: true})
+  }
+
+  handleToggleAnswers = (e) => {
+    e.preventDefault()
+    this.setState({isToggle: false})
+  }
 
   displayAnswers() {
     // function to display only answered question on each user conected
@@ -16,38 +32,47 @@ class Dashboard extends React.Component {
       : null
   }
 
+  displayUnanswers() {
+    // function to display only unanswered question on each user conected
+    const { questionsIds, answeredIds } = this.props
+    return answeredIds
+      ? questionsIds.filter(id => !answeredIds.includes(id)).map(id =>
+        (<li key={id}>
+          {<Question id={id}/>}
+        </li>))
+      : null
+  }
+
   render() {
     return (
       <div className='dashboard'>
         <div className="filter">
           <h1
-            onClick={() => {
-              //set up a function to switch the unanswered to answered questions
-            }}
-            style={{fontWeight: this.props.answeredIds ? '500' : '100'}}>
+            onClick={this.handleToggleAnswers}
+            style={{fontWeight: !this.state.isToggle ? '500' : '100'}}>
             ANSWERED QUESTIONS
           </h1>
           <h1
-            onClick={() => {
-              //set up a function to switch the answered to unanswered questions
-            }}
-            style={{fontWeight: this.props.unansweredIds ? '500' : '100'}}>
+            onClick={this.handleToggleQuestions}
+            style={{fontWeight: this.state.isToggle ? '500' : '100'}}>
             UNANSWERED QUESTIONS
           </h1>
         </div>
         <ul>
-          {this.displayAnswers()}
+          {!this.state.isToggle ? this.displayAnswers() : this.displayUnanswers()}
         </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ authedUser, users }) {
+function mapStateToProps ({ authedUser, users, questions }) {
   const user = authedUser ? Object.values(authedUser) : null
   const answered = user ? users[user].answers : null
+  const questionsIds = Object.keys(questions)
   return {
     answeredIds: answered ? Object.keys(answered) : null,
+    questionsIds: questionsIds,
   }
 }
 
