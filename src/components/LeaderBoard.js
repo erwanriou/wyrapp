d982ff6c5beedef9feb03606f123ab3a1e169340
Erwan Riou <rr.1.riou@gmail.com>
2018-06-08 15:44:34 +0200

    leaderboard setup

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import User from './User.js'
-
 class LeaderBoard extends React.Component {
 
   render() {
     const { users } = this.props
     return (
-      <div className='leaderboard'>
-        <h1>IN PRODUCTION</h1>
-        <ul>
-          {users.map(user => <li>{user.name}</li>)}
-        </ul>
-
-      </div>
+      <table className='leaderboard'>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Profile</th>
+            <th>Questions Asked</th>
+            <th>Questions Answered</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user =>
+            <tr key={user.id}>
+              <td>{users.indexOf(user) + 1}</td>
+              <td>{user.name}</td>
+              <td><img src={user.avatarURL} alt={`Avatar de ${user.name}`}/></td>
+              <td>{user.questions.length}</td>
+              <td>{Object.keys(user.answers).length}</td>
+            </tr>)}
+        </tbody>
+      </table>
     )
   }
 }
 
 function mapStateToProps ({ users, questions }) {
   const usersDisplay = Object.values(users)
+
   return {
-    users: usersDisplay,
+    users: usersDisplay.sort((a, b) =>
+      (Object.keys(b.answers).length + b.questions.length) -
+      (Object.keys(a.answers).length + a.questions.length)
+    ),
     questions,
   }
 }
